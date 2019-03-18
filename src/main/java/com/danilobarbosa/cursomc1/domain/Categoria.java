1c98dc5bf2232403d8f8ddd635e87b20da212e61
danilo-barbosa <danilo_barbosa@hotmail.com>
2019-03-18 16:32:18 -0300

    Banco de dados H2 e criação automática de banco de dados

--- src/main/java/com/danilobarbosa/cursomc1/domain/Categoria.java
+++ src/main/java/com/danilobarbosa/cursomc1/domain/Categoria.java
@@ -2,8 +2,16 @@ package com.danilobarbosa.cursomc1.domain;
 
 import java.io.Serializable;
 
-public class Categoria implements Serializable{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Categoria implements Serializable{
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
