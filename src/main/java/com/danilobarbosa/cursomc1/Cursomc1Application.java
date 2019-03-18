4f202f295c6f364f946999302a6c108138349b0f
danilo-barbosa <danilo_barbosa@hotmail.com>
2019-03-18 13:01:30 -0300

    Projeto Criado

---
+++ src/main/java/com/danilobarbosa/cursomc1/Cursomc1Application.java
@@ -0,0 +1,13 @@
+package com.danilobarbosa.cursomc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursomc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursomc1Application.class, args);
+	}
+
+}
