30b303ec949ea8982e8cb8e5f367bd0ce9428bf4
danilo-barbosa <danilo_barbosa@hotmail.com>
2019-03-18 18:39:17 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/danilobarbosa/cursomc1/domain/Categoria.java
+++ src/main/java/com/danilobarbosa/cursomc1/domain/Categoria.java
@@ -1,19 +1,26 @@
 package com.danilobarbosa.cursomc1.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable{
+	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy="categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {}
 
@@ -37,6 +44,14 @@ public class Categoria implements Serializable{
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
 	}
 	
 	
---
+++ src/main/java/com/danilobarbosa/cursomc1/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.danilobarbosa.cursomc1.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.danilobarbosa.cursomc1.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+	
+}
