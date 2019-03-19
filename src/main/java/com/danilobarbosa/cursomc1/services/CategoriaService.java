78cd6a359503f7879e68a6fa7cfcf03f0b5cae25
danilo-barbosa <danilo_barbosa@hotmail.com>
2019-03-19 12:49:08 -0300

    Ajustes no endpoint /categaorias/{id}

--- src/main/java/com/danilobarbosa/cursomc1/services/CategoriaService.java
+++ src/main/java/com/danilobarbosa/cursomc1/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.danilobarbosa.cursomc1.domain.Categoria;
 import com.danilobarbosa.cursomc1.repositories.CategoriaRepository;
+import com.danilobarbosa.cursomc1.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,7 +18,9 @@ public class CategoriaService {
 	public Categoria find(Integer id) {
 		
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		
 	}
 	
